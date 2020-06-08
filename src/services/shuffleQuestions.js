d25b8484f74e6fd057669d4019b5aa2320c3420e
Hfreitas <hfreitas86@gmail.com>
2020-06-08 18:17:18 -0300

    criada função para randomizar ordem das questões

---
+++ src/services/shuffleQuestions.js
@@ -0,0 +1,15 @@
+/* Recebe uma array, cria uma nova array de
+objetos com uma chave sort randomica 
+(adicionei um segundo Math.random() para
+  garantir que a propriedade sort tenha chances
+  reduzidas de ter o mesmo valor para dois 
+  itens), retorna uma nova array com os itens
+  da array original randomicamente organizados */
+
+const shuffleQuestions = (array) =>
+  array
+    .map((item) => ({ sort: Math.random(), value: item }))
+    .sort((itemA, itemB) => itemA.sort - itemB.sort)
+    .map((item) => item.value);
+
+    export default shuffleQuestions;
