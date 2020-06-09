8538e1abf09350a4b78dd8030f44c2797342a63a
Hfreitas <hfreitas86@gmail.com>
2020-06-08 22:53:17 -0300

    alteração no arquivo endpoints, para comportar requisição às categorias de trivia e detalhar melhor requisição das questoes

--- src/services/endpoints_API.js
+++ src/services/endpoints_API.js
@@ -9,9 +9,25 @@ export const getTriviaToken = async () => {
   return data;
 };
 
+// Obtendo categorias da API Trivia
+export const getTriviaCategories = async () => {
+  const CATEGORIES_URL = 'https://opentdb.com/api_category.php';
+  const response = await fetch(CATEGORIES_URL);
+  const json = await response.json();
+  const data = await await (response.ok
+    ? Promise.resolve(json)
+    : Promise.reject(json));
+  return data;
+};
+
 // Obtendo questões da API Trivia
-export const getTriviaQuestions = async (token) => {
-  const QUESTIONS_URL = `https://opentdb.com/api.php?amount=5&token=${token}`;
+export const getTriviaQuestions = async (
+  token,
+  categoryID,
+  difficulty,
+  type,
+) => {
+  const QUESTIONS_URL = `https://opentdb.com/api.php?amount=5&category=${categoryID}&difficulty=${difficulty}&type=${type}&token=${token}`;
   const response = await fetch(QUESTIONS_URL);
   const json = await response.json();
   const data = await (response.ok
