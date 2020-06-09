8467b1d2ffdddc8066100a74e5e0bf66cf859521
luldelrey <rpgstrikered@gmail.com>
2020-06-09 15:38:56 -0300

    email and name rendered and saved on local state

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,27 +7,53 @@ class Login extends React.Component {
     this.state = {
       email: '',
       name: '',
-    }
+    };
+  }
+
+  handleStateChange(field, value) {
+    this.setState((state) => ({
+      ...state,
+      [field]: value,
+    }));
   }
 
   renderEmail() {
+    const { email } = this.state;
+
     return (
-      <Row label="Name">
-          Nome:&nbsp;
-          <input
-            data-testid="input-player-name"
-            id="input-player-name"
-            onChange={}
-            value={name}
-            />
-        </Row>
+      <label htmlFor="input-gravatar-email">
+        Nome:&nbsp;
+        <input
+          data-testid="input-gravatar-email"
+          id="input-gravatar-email"
+          onChange={(e) => this.handleStateChange('email', e.target.value)}
+          value={email}
+        />
+      </label>
+    );
+  }
+
+  renderName() {
+    const { name } = this.state;
+
+    return (
+      <label htmlFor="input-player-name">
+        Nome:&nbsp;
+        <input
+          data-testid="input-player-name"
+          id="input-player-name"
+          onChange={(e) => this.handleStateChange('name', e.target.value)}
+          value={name}
+        />
+      </label>
     );
   }
 
   render() {
     return (
       <div>
-        
+        {this.renderEmail()}
+        {this.renderName()}
       </div>
     );
   }
