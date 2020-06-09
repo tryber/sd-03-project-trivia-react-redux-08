30599ed856e191215e840385a32980bfc778f185
Hfreitas <hfreitas86@gmail.com>
2020-06-09 14:36:12 -0300

    refatorada page Game para componente de classe, o objetivo é realizar as chamadas de API para obter as questões

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import hashedMail from '../services/encrypt_mail';
 
-function Game({ user, score, email }) {
-  const hash = hashedMail(email);
-  return (
-    <main>
-      <header>
-        <img
-          src={`https://www.gravatar.com/avatar/${hash}?d=https://www.gravatar.com/avatar/2d3bf5b67282f5f466e503d7022abcf3`}
-          alt={`${user} avatar`}
-          data-testid="header-profile-picture"
-        />
-        <span data-testid="header-player-name">{user}</span>
-        <span data-testid="header-score">
-          `Placar:$
-          {score}
-          `
-        </span>
-      </header>
-    </main>
-  );
+class Game extends Component {
+  render() {
+    const { user, score, email } = this.props;
+    const hash = hashedMail(email);
+    return (
+      <main>
+        <header>
+          <img
+            src={`https://www.gravatar.com/avatar/${hash}?d=https://www.gravatar.com/avatar/2d3bf5b67282f5f466e503d7022abcf3`}
+            alt={`${user} avatar`}
+            data-testid="header-profile-picture"
+          />
+          <span data-testid="header-player-name">{user}</span>
+          <span data-testid="header-score">
+            {`Placar:${score}`}
+          </span>
+        </header>
+      </main>
+    );
+  }
 }
 
 Game.propTypes = {
