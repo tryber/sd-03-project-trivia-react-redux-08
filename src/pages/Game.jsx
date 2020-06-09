71fd85d05a666da85698c7fe060b0803fe418392
Hfreitas <hfreitas86@gmail.com>
2020-06-09 14:23:49 -0300

    iniciada implementação da tela de jogo e componente para renderizar perguntas

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import hashedMail from '../services/encrypt_mail';
 
-function Game() {
+function Game({ user, score, email }) {
+  const hash = hashedMail(email);
   return (
     <main>
       <header>
-        <img src={`https://www.gravatar.com/avatar/${hash}?d=https://www.gravatar.com/avatar/2d3bf5b67282f5f466e503d7022abcf3`} alt={`${user} avatar`} data-testid="header-profile-picture" />
+        <img
+          src={`https://www.gravatar.com/avatar/${hash}?d=https://www.gravatar.com/avatar/2d3bf5b67282f5f466e503d7022abcf3`}
+          alt={`${user} avatar`}
+          data-testid="header-profile-picture"
+        />
         <span data-testid="header-player-name">{user}</span>
         <span data-testid="header-score">
           `Placar:$
@@ -15,5 +22,11 @@ function Game() {
     </main>
   );
 }
+
+Game.propTypes = {
+  email: PropTypes.string.isRequired,
+  score: PropTypes.number.isRequired,
+  user: PropTypes.string.isRequired,
+};
 
 export default Game;
