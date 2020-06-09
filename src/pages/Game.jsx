761c1b0ff244d010bf49f7a35835816e2098dd25
Hfreitas <hfreitas86@gmail.com>
2020-06-09 12:07:52 -0300

    iniciado requisito 1 - header

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 function Game() {
   return (
-    <div>
-      <h1>Game</h1>
-    </div>
+    <main>
+      <header>
+        <img src={`https://www.gravatar.com/avatar/${hash}?d=https://www.gravatar.com/avatar/2d3bf5b67282f5f466e503d7022abcf3`} alt={`${user} avatar`} data-testid="header-profile-picture" />
+        <span data-testid="header-player-name">{user}</span>
+        <span data-testid="header-score">
+          `Placar:$
+          {score}
+          `
+        </span>
+      </header>
+    </main>
   );
 }
 
