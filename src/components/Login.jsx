5a3a42fa2706ccf01b8024e9e617d42bf6c50451
luldelrey <rpgstrikered@gmail.com>
2020-06-10 15:02:49 -0300

    connect function imported but not implemented

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+
 import '../styles/Login.css';
 
 class Login extends React.Component {
