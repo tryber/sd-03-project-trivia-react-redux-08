d2e310fb7173a44e4aad10423c05a565716992f7
Hfreitas <hfreitas86@gmail.com>
2020-06-10 16:00:06 -0300

    logica do timer em implementação

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -32,6 +32,14 @@ class Game extends Component {
       : { questionIndex: 0 }));
   }
 
+  timerStarter() 
+    const { timer } = this.state;
+    const startedTimer = setTimeout(() => {
+      this.setState((state) => ({timer: state.timer - 1}))
+    }, 1000);
+    return timer <= 30 && timer > 0 ?
+  }
+
   render() {
     const { questionIndex } = this.state;
     const {
