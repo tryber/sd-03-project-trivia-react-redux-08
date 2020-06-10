10e5db80a8581be80427c8868303f1a130314081
Hfreitas <hfreitas86@gmail.com>
2020-06-10 16:05:47 -0300

    Incorporando page Home

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-
 import setUserInfo from '../actions/actionsCreators';
 import '../styles/Login.css';
 
