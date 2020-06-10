a4c0345c9e09b64ac3aae53b9aebf6f925e823b6
Gabriel-O-Ferreira <gabriel1335555@gmail.com>
2020-06-10 17:17:57 -0300

    Modyfing Header Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import hashedMail from '../services/encrypt_mail';
+import '../styles/Header.css'
 
-function Header () {
+function Header(props) {
+  const { name, score, email } = props;
+  const hash = hashedMail(email);
   return (
-    <header>
-      <p>UserInfo</p>
-    </header>
+    <div className="header-feedback-and-game">
+      <div>
+        <img
+          src={`https://www.gravatar.com/avatar/${hash}?d=https://www.gravatar.com/avatar/2d3bf5b67282f5f466e503d7022abcf3`}
+          alt={`${name} avatar`}
+          data-testid="header-profile-picture"
+        />
+      </div>
+      <div>
+        <span data-testid="header-player-name">{name}aa</span>
+        <span data-testid="header-score">{`Placar:${score}`}</span>
+      </div>
+    </div>
   );
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  data: state.userInfo
+})
+
+export default connect(mapStateToProps)(Header);
