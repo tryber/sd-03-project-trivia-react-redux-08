af5b76c75beaab06cea9d89016e4694fc285dc82
Gabriel-O-Ferreira <gabriel1335555@gmail.com>
2020-06-10 16:20:05 -0300

    Adding Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header () {
+  return (
+    <header>
+      <p>UserInfo</p>
+    </header>
+  );
+}
+
+export default Header;
--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function Feedback() {
   return (
     <div>
       <h1>Feedback</h1>
+      <Header />
     </div>
   );
 }
