87dc2639212d4c218286287b0fa94db2aee4e7b0
Gabriel-O-Ferreira <gabriel1335555@gmail.com>
2020-06-12 19:57:22 -0300

    codeclimate1

--- src/components/FeedBackMsg.jsx
+++ src/components/FeedBackMsg.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import PropTypes from 'prop-types';
 
 const FeedBackMsg = ({ assertions }) => (assertions >= 3 ? (
   <p data-testid="feedback-text">Mandou bem!</p>
 ) : (
-  <p data-testid="feedback-text">Podia ser melhor!</p> 
-)
-);
+  <p data-testid="feedback-text">Podia ser melhor!</p>
+));
+
+FeedBackMsg.prototype = {
+  assertions: PropTypes.number.isRequired,
+}
 
 const mapStateToProps = ({ gameInfo }) => ({
-  assertions: gameInfo.assertions 
-})
+  assertions: gameInfo.assertions,
+});
 
 export default connect(mapStateToProps)(FeedBackMsg);
