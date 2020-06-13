95a3e31ee7aef8d8eaa97fdf3e5d318fcd134f89
Gabriel-O-Ferreira <gabriel1335555@gmail.com>
2020-06-13 20:01:30 -0300

    data-testid add

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 
 const Results = ({ score, assertions }) => (
   <div>
-    <h4>{`Você acertou ${assertions} questões!`}</h4>
-    <h4>{`Um total de ${score} pontos`}</h4>
+    <h4 data-testid="feedback-total-question" >{`Você acertou ${assertions} questões!`}</h4>
+    <h4 data-testid="feedback-total-score">{`Um total de ${score} pontos`}</h4>
   </div>
 );
 
