4f2e4088071b79aa23d9920dc9778a1cfefee2f4
luldelrey <rpgstrikered@gmail.com>
2020-06-15 17:04:26 -0300

    initial commit

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Header from '../components/Header';
+import Rank from '../components/Rank';
 
-function Ranking() {
-  return (
-    <div>
-      <h1>Ranking</h1>
-    </div>
-  );
+function Ranking({ isLogged }) {
+  if (isLogged) {
+    return (
+      <div>
+        <h1>Ranking</h1>
+        <Header />
+        <Rank />
+      </div>
+    );
+  }
+  return (<h1><Link to="/">Oops! Please, log to play!</Link></h1>);
 }
 
-export default Ranking;
+Ranking.propTypes = {
+  isLogged: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = (state) => ({ isLogged: state.userInfo.isLogged });
+
+export default connect(mapStateToProps)(Ranking);
