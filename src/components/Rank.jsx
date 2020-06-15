b61d2cb812a9f6dcebaebd52218cf7987ef28197
luldelrey <rpgstrikered@gmail.com>
2020-06-15 17:31:46 -0300

    rank added

---
+++ src/components/Rank.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+class Rank extends React.Component {
+  render() {
+    return (
+      <div>
+        <Link
+          data-testid="btn-go-home"
+          to="/"
+        >
+          Início
+        </Link>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  islogged: state.userInfo.isLogged,
+});
+
+export default connect(mapStateToProps)(Rank);
