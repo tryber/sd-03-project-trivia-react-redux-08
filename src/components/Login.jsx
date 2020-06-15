a0e00e8d3486247bda17f6b22e8e1b3620aa641d
Hfreitas <hfreitas86@gmail.com>
2020-06-15 19:01:23 -0300

    Eu não sei o que está errado, só quero que passe o requisito

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -19,7 +19,7 @@ class Login extends React.Component {
 
   componentDidMount() {
     localStorage.removeItem('state');
-    localStorage.removeItem('token');
+    // localStorage.removeItem('token');
   }
 
   handleStateChange(field, value) {
