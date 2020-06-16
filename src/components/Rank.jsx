98d001dbaf5b5d28edf44405bad3900b9d495497
luldelrey <rpgstrikered@gmail.com>
2020-06-16 17:12:40 -0300

    start do display rank

--- src/components/Rank.jsx
+++ src/components/Rank.jsx
@@ -3,16 +3,26 @@ import { connect } from 'react-redux';
 
 class Rank extends React.Component {
   render() {
+    const rank = JSON.parse(localStorage.getItem('ranking'));
+    console.log(rank[0]);
     return (
       <div>
-        <p>placeholder</p>
+        <ol>
+          {rank.map((player, index) => (
+            <li key={`${player.name}_${player.score}`}>
+              <img src={player.picture} alt="player pic" />
+              <h3 data-testid={`player-name-${index}`}>{player.name}</h3>
+              <h4 data-testid={`player-score-${index}`}>{player.score}</h4>
+            </li>
+          ))}
+        </ol>
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  islogged: state.userInfo,
+  islogged: state,
 });
 
 export default connect(mapStateToProps)(Rank);
